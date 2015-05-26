52b69a15ce85420780abd993e0088c05426c3b86
Sklicey <sking@synensishealth.com>
2015-05-26 22:22:48 +0000

    all

--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 20150521185306) do
 
   create_table "tweets", force: :cascade do |t|
     t.text     "content"
-    t.integer  "member_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  
-end
+  end
+
 end
